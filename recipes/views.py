1a17d2b89abb24ffbd455d681369951891fd0dcf
erikorego <erikorego@outlook.com>
2023-01-13 17:04:29 -0300

    Started app recipes

---
+++ recipes/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def home(request):
+    return HttpResponse('Home')
+
+def contato(request):
+    return HttpResponse('contato')
+
+def sobre(request):
+    return HttpResponse('sobre')
+# Create your views here.
